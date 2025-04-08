78fe26a4d7177831725b49f5e2250780610b9bf8
DonConMan <194740509+wfsutherland@users.noreply.github.com>
2025-04-08 11:33:04 -0400

    Added gradebook html and js files
    Part of Lesson 12

---
+++ Lesson 12/gradebook.js
@@ -0,0 +1,17 @@
+//TODO: Fetch data from PostgreSQL DB (implement later)
+function fetchGradeData() {
+    //Query db and return grade data
+    console.log("Fetching grade data...")
+}
+
+//TODO: populate the table with grade data
+function populateGradebook(data){
+    //take the fetched gade data and populate table
+    console.log("Populating gradebook with data:", data)
+}
+
+//TODO: REMOVE THIS
+//Call stubs to denostrate workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+//END REMOVE
